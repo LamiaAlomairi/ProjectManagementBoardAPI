89296e24818f5518e9bd92d542b268742183a854
Lamia <lama5alomairi@gmail.com>
2023-08-01 19:49:07 +0400

    Create SectionRepository with extends

---
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Repositories/SectionRepository.java
@@ -0,0 +1,7 @@
+package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Repositories;
+
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<Section, Integer> {
+}
