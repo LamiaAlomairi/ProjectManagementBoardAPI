73da32aa59f40ffb5c223848214f774a19e24cf1
Lamia <lama5alomairi@gmail.com>
2023-08-01 12:19:06 +0400

    Add extends to BoardRepository interface

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Repositories/BoardRepository.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Repositories/BoardRepository.java
@@ -1,4 +1,7 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Repositories;
 
-public interface BoardRepository {
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, String> {
 }
