27f6d976fc1ee6c1eef7529178228f3fc64f12a6
Lamia <lama5alomairi@gmail.com>
2023-08-01 12:06:04 +0400

    Create ProjectManagementBoardAPI project

---
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/ProjectManagementBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBoardApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/ProjectManagementBoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBoardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
