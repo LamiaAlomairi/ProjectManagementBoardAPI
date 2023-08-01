30caaba7f871a920a072117f5d8064fbff26bc8a
Lamia <lama5alomairi@gmail.com>
2023-08-01 14:56:38 +0400

    Add query to get board by id

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Repositories/BoardRepository.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Repositories/BoardRepository.java
@@ -3,10 +3,14 @@ package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Repositories;
 import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, String> {
     @Query(value = "select b from Board b")
     List<Board> getAllBoards();
+
+    @Query(value = "select b from Board b where b.id = :boardId")
+    Board getBoardById(@Param("boardId") String id);
 }
