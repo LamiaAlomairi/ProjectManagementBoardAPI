67b6bf4d0724661acb49d1cb04c749b18d489186
Lamia <lama5alomairi@gmail.com>
2023-08-01 14:05:50 +0400

    Codes have been added to the BoardResponseObject class

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/ResponseObject/BoardResponseObject.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/ResponseObject/BoardResponseObject.java
@@ -1,4 +1,35 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.ResponseObject;
 
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Board;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+@Builder
 public class BoardResponseObject {
+    String id;
+    String title;
+    public static BoardResponseObject convertToResponse(Board board) {
+        return BoardResponseObject.builder()
+                .id(board.getId())
+                .title(board.getTitle())
+                .build();
+    }
+
+    public static List<BoardResponseObject> convertToResponseList(List<Board> response) {
+        List<BoardResponseObject> boardResponseObjects = new ArrayList<>();
+        if (!response.isEmpty()) {
+            for (Board board : response) {
+                boardResponseObjects.add(convertToResponse(board));
+            }
+        }
+        return boardResponseObjects;
+    }
 }
