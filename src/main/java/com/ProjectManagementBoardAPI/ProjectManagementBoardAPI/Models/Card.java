f45aa785c522886ea272d450d3c067a679e19a63
Lamia <lama5alomairi@gmail.com>
2023-08-01 12:15:39 +0400

    Add attributes to the Card class

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Models/Card.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Models/Card.java
@@ -1,4 +1,17 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "card")
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String title;
+    String description;
 }
