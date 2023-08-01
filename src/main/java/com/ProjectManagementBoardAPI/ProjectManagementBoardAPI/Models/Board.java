f494b9fc05df4b59428a0c56eac28f3c736211a1
Lamia <lama5alomairi@gmail.com>
2023-08-01 12:12:57 +0400

    Add attributes to the Board class

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Models/Board.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Models/Board.java
@@ -1,4 +1,17 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "board")
 public class Board {
+    @Id
+    String id;
+    String title;
 }
