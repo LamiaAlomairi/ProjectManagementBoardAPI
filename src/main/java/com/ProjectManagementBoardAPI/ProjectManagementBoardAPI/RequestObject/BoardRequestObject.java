89008d9a996670031003204b6e90078e58bd6f96
Lamia <lama5alomairi@gmail.com>
2023-08-01 12:38:20 +0400

    Codes have been added to the BoardRequestObject class

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/RequestObject/BoardRequestObject.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/RequestObject/BoardRequestObject.java
@@ -1,4 +1,40 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.RequestObject;
 
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Board;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
 public class BoardRequestObject {
+    String id;
+    String title;
+
+    public static Board convert(BoardRequestObject request) {
+        Date nowDate = new Date();
+        Board board = new Board();
+
+        board.setId(request.getId());
+        board.setTitle(request.getTitle());
+        board.setIsActive(true);
+        board.setCreatedDate(nowDate);
+        board.setUpdatedDate(nowDate);
+        return board;
+    }
+
+    public static List<Board> convert(List<BoardRequestObject> requestList) {
+        List<Board> boards = new ArrayList<>();
+        if (!requestList.isEmpty()) {
+            for (BoardRequestObject boardRequestObject : requestList) {
+                boards.add(convert(boardRequestObject));
+            }
+        }
+        return boards;
+    }
 }
