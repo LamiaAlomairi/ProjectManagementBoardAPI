7868afe365aa4fee26d25d721426a62da543598a
Lamia <lama5alomairi@gmail.com>
2023-08-01 14:14:02 +0400

    The addBoard function has been added to the BoardService class

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Services/BoardService.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Services/BoardService.java
@@ -1,4 +1,18 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Services;
 
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Board;
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Repositories.BoardRepository;
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.RequestObject.BoardRequestObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoardService {
+    @Autowired
+    BoardRepository boardRepository;
+
+    public void addBoard(BoardRequestObject boardRequest){
+        Board board = BoardRequestObject.convert(boardRequest);
+        boardRepository.save(board);
+    }
 }
