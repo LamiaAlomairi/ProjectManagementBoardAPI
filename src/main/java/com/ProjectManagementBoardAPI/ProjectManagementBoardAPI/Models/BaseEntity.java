05aad7f9221f66645adf78158d50457c7c20d720
Lamia <lama5alomairi@gmail.com>
2023-08-01 12:30:14 +0400

    Add attributes to the BaseEntity class

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Models/BaseEntity.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Models/BaseEntity.java
@@ -1,4 +1,22 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+@Setter
+@Getter
+@MappedSuperclass
 public class BaseEntity {
+    Boolean isActive;
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Muscat")
+    Date createdDate;
+    @UpdateTimestamp
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Muscat")
+    Date updatedDate;
 }
