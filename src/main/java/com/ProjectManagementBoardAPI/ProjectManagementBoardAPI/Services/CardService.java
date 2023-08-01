8cb0fd0cfb460ce101b710b87f86cda34e216ec1
Lamia <lama5alomairi@gmail.com>
2023-08-01 16:55:04 +0400

    Add function createCard to CardService

--- src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Services/CardService.java
+++ src/main/java/com/ProjectManagementBoardAPI/ProjectManagementBoardAPI/Services/CardService.java
@@ -1,4 +1,22 @@
 package com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Services;
 
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Models.Card;
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.Repositories.CardRepository;
+import com.ProjectManagementBoardAPI.ProjectManagementBoardAPI.RequestObject.CardRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CardService {
+    @Autowired
+    CardRepository cardRepository;
+
+    /*******  Create Card  ******/
+    public void createCard(CardRequest cardRequest){
+        try{
+            Card card = CardRequest.convert(cardRequest);
+            cardRepository.save(card);
+        }
+        catch (Exception e) {
+            System.out.println("Cannot create card " + e.getMessage());
+        }
+    }
 }
